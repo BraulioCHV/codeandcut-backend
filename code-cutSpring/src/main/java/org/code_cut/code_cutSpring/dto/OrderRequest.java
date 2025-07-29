5e5274826ac074e78e69b47aa612d1636adc59d1
unknown <franciscojavier.gutierrezrdgz@gmail.com>
2025-07-29 12:50:28 -0600

    cambios archivo OrderRequest

--- code-cutSpring/src/main/java/org/code_cut/code_cutSpring/dto/OrderRequest.java
+++ code-cutSpring/src/main/java/org/code_cut/code_cutSpring/dto/OrderRequest.java
@@ -1,11 +1,4 @@
 package org.code_cut.code_cutSpring.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class OrderRequest {
-    private String address;
-    private Double priceAmount;
 }
