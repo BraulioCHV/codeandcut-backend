ec81634f8d628ae81dc38b858a9052033a3944d6
Ellen Hernandez <ellenjarely.ht@gmail.com>
2025-07-29 16:37:37 -0600

    breve corrección en DetailsOrderController

--- code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/DetailsOrderServiceImpl.java
+++ code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/DetailsOrderServiceImpl.java
@@ -12,8 +12,8 @@ public class DetailsOrderServiceImpl implements DetailsOrderService {
     private final DetailsOrderRepository detailsOrderRepository;
 
     @Autowired
-    public DetailsOrderServiceImpl(DetailsOrderRepository detalleOrdenRepository) {
-        this.detailsOrderRepository = detalleOrdenRepository;
+    public DetailsOrderServiceImpl(DetailsOrderRepository detailsOrderRepository) {
+        this.detailsOrderRepository = detailsOrderRepository;
     }
 
     @Override
@@ -23,13 +23,13 @@ public class DetailsOrderServiceImpl implements DetailsOrderService {
 
     @Override
     public DetailsOrder findById(Long id) {
-        return detailsOrderRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Detalle de orden no encontrado con ID: " + id));
+        return detailsOrderRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Detalle de orden no encontrado con ID: " + id));
     }
 
     @Override
     public DetailsOrder save(DetailsOrder detailsOrder) {
-        return (DetailsOrder) detailsOrderRepository.save(detailsOrder);
+        return detailsOrderRepository.save(detailsOrder);
     }
 
     @Override
@@ -38,7 +38,7 @@ public class DetailsOrderServiceImpl implements DetailsOrderService {
     }
 
     @Override
-    public List<DetailsOrder> findByPedidoId(Long idOrder) {
-        return detailsOrderRepository.findByOrder_Id(idOrder);
+    public List<DetailsOrder> findByOrderId(Long orderId) {
+        return detailsOrderRepository.findByOrder_Id(orderId);
     }
 }
