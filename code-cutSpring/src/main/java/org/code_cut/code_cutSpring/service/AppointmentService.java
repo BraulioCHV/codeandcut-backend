0ad8ffdd4b38ea5991a6a00b1b75125a6fe7b205
Sarai <saraimontes170@gmail.com>
2025-07-31 21:04:03 -0600

    conflicto resuelto

--- code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/AppointmentService.java
+++ code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/AppointmentService.java
@@ -6,6 +6,8 @@ import org.code_cut.code_cutSpring.model.Appointment;
 import org.code_cut.code_cutSpring.model.Employee;
 import org.code_cut.code_cutSpring.model.Services;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface AppointmentService {
@@ -13,7 +15,11 @@ public interface AppointmentService {
     Appointment createAppointment(Appointment appointment);
     Appointment updateAppointmentById(Long id, Appointment appointment);
     void deleteAppointment(Long id);
-    Appointment addServiceAndEmployee(Long id, Services serivce, Employee employee);
+    Appointment addServiceAndEmployee(Long id, Services service, Employee employee);
+
+
+
+
 
     // Appointment createAppointment(AppointmentRequest request);
     //void deleteAppointment(Long id);
