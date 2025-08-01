e94806a38bb0722d5144481bacdaca4119d9df9e
Alfonso Esau <kernelsoundstudio@gmail.com>
2025-08-01 07:05:22 -0600

    minor hotfix

--- code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/OrdersServiceImpl.java
+++ code-cutSpring/src/main/java/org/code_cut/code_cutSpring/service/OrdersServiceImpl.java
@@ -84,7 +84,9 @@ public class OrdersServiceImpl implements OrdersService{
         Payment payment = new Payment();
         payment.setTypepayment(paymentRequest.getTypePayment());
         payment.setStatus(paymentRequest.getStatus());
+
         payment.setOrders(order);
+        order.setPayment(payment);
 
 
         paymentRepository.save(payment);
